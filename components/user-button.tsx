dfd49737195a0b8ab5775f5e56ccdce06fc1d741
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2024-10-07 21:41:35 -0300

    final commit

---
+++ components/user-button.tsx
@@ -0,0 +1,53 @@
+import { auth } from "@/auth"
+import { SignInGoogleButton, SignOut } from "@/components/auth-components"
+import { Avatar, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export default async function UserButton() {
+  const session = await auth()
+  if (!session?.user) return <SignInGoogleButton />
+  return (
+    <div className="flex items-center gap-2">
+      <span className="hidden text-sm sm:inline-flex">
+        {session.user.email}
+      </span>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+            <Avatar className="h-8 w-8">
+              <AvatarImage
+                src={
+                  session.user.image ??
+                  `https://api.dicebear.com/9.x/thumbs/svg?seed=${Math.floor(Math.random() * 100000) + 1}&randomizeIds=true`
+                }
+                alt={session.user.name ?? ""}
+              />
+            </Avatar>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56" align="end" forceMount>
+          <DropdownMenuLabel className="font-normal">
+            <div className="flex flex-col space-y-1">
+              <p className="text-sm font-medium leading-none">
+                {session.user.name}
+              </p>
+              <p className="text-muted-foreground text-xs leading-none">
+                {session.user.email}
+              </p>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuItem>
+            <SignOut />
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  )
+}
